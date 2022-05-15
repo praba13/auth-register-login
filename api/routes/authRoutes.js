d375de61882af2b062267f53ae95c54b7143e70e
Praba <prama1303@gmail.com>
2022-05-15 07:14:43 +0200

    very simple Register finshed

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import User from '../models/UserModel.js';
 const route = express.Router();
 
+//Register
 route.post('/addUser', async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
     const user = await new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password
+      password: hashPassword
     });
     const users = await user.save();
     !users && res.status(404).send('Not Created');
